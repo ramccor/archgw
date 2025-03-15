31d08ec0d39ab99699aa670bb84cf7958a0bf1a8
Adil Hafeez <adil.hafeez@gmail.com>
2025-03-14 17:39:45 -0700

    add tests

--- demos/shared/test_runner/run_demo_tests.sh
+++ demos/shared/test_runner/run_demo_tests.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # for demo in currency_exchange hr_agent
-for demo in currency_exchange
+for demo in currency_exchange weather_forecast
 do
   echo "******************************************"
   echo "Running tests for $demo ..."
@@ -11,7 +11,7 @@ do
   archgw up arch_config.yaml
   docker compose up -d
   cd ../../shared/test_runner
-  TEST_DATA=../../samples_python/$demo/test_data.yaml poetry run pytest
+  hurl --test hurl_tests
   cd ../../samples_python/$demo
   archgw down
   docker compose down -v
