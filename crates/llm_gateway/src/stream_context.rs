49342c3c42e641399aff8a87e2cbe378b9280037
Adil Hafeez <adil.hafeez@gmail.com>
2025-03-25 21:02:54 -0700

    fix tests

--- crates/llm_gateway/src/stream_context.rs
+++ crates/llm_gateway/src/stream_context.rs
@@ -220,7 +220,10 @@ impl HttpContext for StreamContext {
             }));
         } else {
             self.select_llm_provider();
-            self.add_http_request_header(ARCH_ROUTING_HEADER, &self.llm_provider().name);
+            self.add_http_request_header(
+                ARCH_ROUTING_HEADER,
+                &self.llm_provider().provider_interface.to_string(),
+            );
             if let Err(error) = self.modify_auth_headers() {
                 // ensure that the provider has an endpoint if the access key is missing else return a bad request
                 if self.llm_provider.as_ref().unwrap().endpoint.is_none() && !use_agent_orchestrator
