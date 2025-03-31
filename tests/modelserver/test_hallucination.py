ddf5f23973fa8c5673d2ec719578029ab3fbd556
co tran <cotran2@utexas.edu>
2025-03-31 23:14:10 +0000

    remove test until more evaluation on example

--- tests/modelserver/test_hallucination.py
+++ tests/modelserver/test_hallucination.py
@@ -23,22 +23,22 @@ with open(yaml_file_path, "r") as file:
     test_data_yaml = yaml.safe_load(file)
 
 
-@pytest.mark.parametrize(
-    "test_data",
-    [
-        pytest.param(test_case, id=test_case["id"])
-        for test_case in test_data_yaml["test_cases"]
-    ],
-)
-def test_model_server(test_data):
-    input = test_data["input"]
-    expected = test_data["expected"]
+# @pytest.mark.parametrize(
+#     "test_data",
+#     [
+#         pytest.param(test_case, id=test_case["id"])
+#         for test_case in test_data_yaml["test_cases"]
+#     ],
+# )
+# def test_model_server(test_data):
+#     input = test_data["input"]
+#     expected = test_data["expected"]
 
-    response = requests.post(MODEL_SERVER_ENDPOINT, json=input)
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "application/json"
+#     response = requests.post(MODEL_SERVER_ENDPOINT, json=input)
+#     assert response.status_code == 200
+#     assert response.headers["content-type"] == "application/json"
 
-    response_json = response.json()
-    assert response_json
-    metadata = response_json.get("metadata", {})
-    assert (metadata["hallucination"].lower() == "true") == expected[0]["hallucination"]
+#     response_json = response.json()
+#     assert response_json
+#     metadata = response_json.get("metadata", {})
+#     assert (metadata["hallucination"].lower() == "true") == expected[0]["hallucination"]
